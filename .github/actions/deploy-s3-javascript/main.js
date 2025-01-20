a45e7d82fda958d3cb29caac4ebb3bcd158766d1
jelly3105 <anjalisherikar3105@gmail.com>
2025-01-20 18:28:21 +0530

    deploy to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,15 @@
 const core = require('@actions/core');
+const exec  = require('@actions/exec');
 
 function run() {
+    // 1. Get input values
+    const bucket = core.getInput('bucket', {required :  true});
+    const bucketRegion = core.getInput('bucket-region', {required :  true});
+    const distFolder = core.getInput('dist-folder', {required :  true});
+
+    // 2. Upload files
+    const S3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${S3Uri} --region ${bucketRegion}`);
     core.notice('Hello World!')
 }
 
